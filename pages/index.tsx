fa987716179f8c3f9e32b0ee213cad2346076f95
suin <suinyeze@gmail.com>
2023-03-03 15:46:06 +0900

    feat: 🎸 ページを表示したときに画像を表示する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+  // ❶ useStateを使って状態を定義する
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  // ❷ マウント時に画像を読み込む宣言
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url); // 画像URLの状態を更新する
+      setLoading(false); // ローディング状態を更新する
+    });
+  }, []);
+  // ❸ ローディング中でなければ、画像を表示する
+  return <div>{loading || <img src={imageUrl} />}</div>;
 };
 export default IndexPage;
 

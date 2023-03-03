5d6aa4137a9958594617d342514b5985e4b71fac
suin <suinyeze@gmail.com>
2023-03-03 15:37:29 +0900

    feat: 🎸 画像を取得する関数を実装する

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,3 +4,10 @@ const IndexPage: NextPage = () => {
   return <div>猫画像予定地</div>;
 };
 export default IndexPage;
+
+const fetchImage = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  console.log(images);
+  return images[0];
+};

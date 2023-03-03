ceff88a936d00f02a05823241a9ec675c87e6377
suin <suinyeze@gmail.com>
2023-03-03 16:26:08 +0900

    feat: 🎸 ビジュアルを作り込む

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useState } from "react";
+import styles from "./index.module.css";
 
 type Props = {
   initialImageUrl: string;
@@ -15,9 +16,13 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     setLoading(false);
   };
   return (
-    <div>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
-      <div>{loading || <img src={imageUrl} />}</div>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
     </div>
   );
 };

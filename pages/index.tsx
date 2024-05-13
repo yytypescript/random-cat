094b74289b749906b7ae8748517f758ff1c1970d
Nishimoto <nnishimoto@mxdlab.net>
2024-05-13 23:26:56 +0900

    Update index.tsx
    updatte

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        One　more catß
+        なんか上手くいかねえ
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}

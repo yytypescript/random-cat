96dd579a6faf08fdb33f40803ddd9064a4e98281
Ito Yoshiyuki <yoshiyasu.ito@K0HN76VVGH.local>
2024-11-16 11:03:25 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他のにゃんこも見る
+        他のにゃんこも見る（しゅんくんだよ！）
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
